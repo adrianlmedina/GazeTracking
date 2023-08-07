a0fdc0e627c7b6789ec84d80aa222a4a3e5dc3fb
Jennifer Fadul <jfadul22@gmail.com>
2023-08-07 16:01:29 -0700

    saliency mapping added to code

--- example.py
+++ example.py
@@ -64,8 +64,44 @@ while True: #img_counter < max_images:
         im = ImageGrab.grab(bbox=bbox)
         im.save(f"filename_specific_{specific_counter + 1}.png")
         print("Screenshot {} taken from specific location!".format(specific_counter + 1))
-        specific_counter += 1
-        
+        import cv2
+        import matplotlib.pyplot as plt
+        import pySaliencyMap
+
+        # saliency mapping
+        if __name__ == '__main__':
+            # read
+            img = cv2.imread(f"filename_specific_{specific_counter + 1}.png")
+            # initialize
+            imgsize = img.shape
+            img_width  = imgsize[1]
+            img_height = imgsize[0]
+            sm = pySaliencyMap.pySaliencyMap(img_width, img_height)
+            # computation
+            saliency_map = sm.SMGetSM(img)
+            binarized_map = sm.SMGetBinarizedSM(img)
+            salient_region = sm.SMGetSalientRegion(img)
+    
+            # visualize
+        #    plt.subplot(2,2,1), plt.imshow(img, 'gray')
+            plt.subplot(2,2,1), plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+            plt.title('Input image')
+        #    cv2.imshow("input",  img)
+            plt.subplot(2,2,2), plt.imshow(saliency_map, 'gray')
+            plt.title('Saliency map')
+        #    cv2.imshow("output", map)
+            plt.subplot(2,2,3), plt.imshow(binarized_map)
+            plt.title('Binarilized saliency map')
+        #    cv2.imshow("Binarized", binarized_map)
+            plt.subplot(2,2,4), plt.imshow(cv2.cvtColor(salient_region, cv2.COLOR_BGR2RGB))
+            plt.title('Salient region')
+        #    cv2.imshow("Segmented", segmented_map)
+
+            plt.show()
+        #        cv2.waitKey(0)
+            cv2.destroyAllWindows()
+            specific_counter += 1
+
     elif key == 32 and fullscreen_counter < max_images:
         _, full_frame = webcam.read()  # Capture the entire frame from the webcam
         im = Image.fromarray(cv2.cvtColor(full_frame, cv2.COLOR_BGR2RGB))
